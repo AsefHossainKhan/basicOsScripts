63b658eed3eb9bf402882568f18abcc9c16cceb5
AsefHossainKhan <56684655+AsefHossainKhan@users.noreply.github.com>
2020-09-11 15:52:30 +0600

    changed syntax

--- lab task 2/task1.bash
+++ lab task 2/task1.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 read -p "What is your age? " age
-read -p "Was your grade in physics/chemistry above A? [yes/no] " grade
+read -p "Was your grade in physics/chemistry above or equal to A? [yes/no] " grade
 
 if [ $grade == "yes" ] && [ $age -ge 18 ]
 then
